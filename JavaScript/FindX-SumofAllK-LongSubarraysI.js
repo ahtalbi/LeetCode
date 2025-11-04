1f347e2045391e4ff58c40cbe93f548e72e4dc46
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-11-04 20:57:31 +0100

    FindX-SumofAllK-LongSubarraysI

--- JavaScript/FindX-SumofAllK-LongSubarraysI.js
+++ JavaScript/FindX-SumofAllK-LongSubarraysI.js
@@ -1,23 +1,22 @@
 var findXSum = function(nums, k, x) {
     const n = nums.length;
-    let freq = new Map(), res = [];
-    for (let i = 0; i <= n-k; i++) {
-        if (i === 0) {
-            for (let j = 0; j < k; j++) freq.set(nums[j], (freq.get(nums[j]) || 0) + 1);
-        } else {
-            freq.set(nums[i-1], freq.get(nums[i-1]) - 1);
-            if (i + k - 1 < n) freq.set(nums[i+k-1], (freq.get(nums[i+k-1]) || 0) + 1);
+    const freq = new Map();
+    const res = [];
+
+    for (let i = 0; i < n; i++) {
+        freq.set(nums[i], (freq.get(nums[i]) || 0) + 1);
+        if (i >= k) {
+            const prev = nums[i - k];
+            freq.set(prev, freq.get(prev) - 1);
+            if (freq.get(prev) === 0) freq.delete(prev);
         }
-
-        let top = [...freq.entries()]
-                    .filter(([_, val]) => val > 0)
-                    .sort((a, b) => b[1] - a[1] || b[0] - a[0])
-                    .slice(0, x);
-        res.push(top.reduce((acc, [key, val]) => acc + val * key, 0));
+        if (i >= k - 1) {
+            let arr = Array.from(freq.entries());
+            arr.sort((a,b) => b[1] - a[1] || b[0] - a[0]);
+            res.push(arr.slice(0, x).reduce((acc,[key,val]) => acc + key * val, 0));
+        }
     }
-
     return res;
 };
-
 
 console.log(findXSum([1,1,2,2,3,4,2,3], 6, 2));
